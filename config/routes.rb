7c998ae0a58c9c58aae8b326c2171f9ecf171e84
Sheldon Johnson <shayolden@hotmail.com>
2020-09-28 01:21:22 +1000

    Add players screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'ratings#index'
 
+  resources :players, only: [:index]
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :matches, only: [:create]
