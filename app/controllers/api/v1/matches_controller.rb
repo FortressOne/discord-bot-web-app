1de518fae9312a5a4df803c5d433bdaa63a54ed1
Sheldon Johnson <shayolden@hotmail.com>
2020-09-28 01:05:37 +1000

    Nasty second pass at API request

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -2,23 +2,33 @@ class Api::V1::MatchesController < ActionController::API
   def create
     match = Match.create
 
-    params[:match][:teams].each do |name, attrs|
+    match_params[:teams].each do |name, attrs|
       team = match.teams.new
 
+      team.result = if attrs[:winner] == true
+                      1
+                    else
+                      -1
+                    end
+
       attrs[:players].each do |discord_id|
-        player = Player.find_or_create_by(discord_id: discord_id)
+        player = Player.find_or_create_by(
+          discord_id: discord_id
+        )
+
         team.players << player
       end
 
       team.save
     end
 
-    if params[:match][:map]
-      match.game_map = GameMap.find_or_create_by(name: params[:match][:map])
-    end
-
+    match.game_map = GameMap.find_or_create_by(name: match_params[:map]) if match_params[:map]
     match.save
 
     render json: match.to_json, status: :ok
+  end
+
+  def match_params
+    params.require(:match).permit(:map, { teams: {} }) 
   end
 end
