cff1c8f107cfff0036947eaac7dd2d653cfaeefa
Sheldon Johnson <shayolden@hotmail.com>
2020-09-29 23:44:21 +1000

    Refactor

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -5,30 +5,27 @@ class Api::V1::MatchesController < ActionController::API
     match_params[:teams].each do |name, attrs|
       team = match.teams.new
 
-      team.result = if attrs[:winner] == true
-                      1
-                    else
-                      -1
-                    end
-
       attrs[:players].each do |discord_id|
-        player = Player.find_or_create_by(
-          discord_id: discord_id
-        )
-
-        team.players << player
+        team.players << Player.find_or_create_by(discord_id: discord_id)
       end
 
+      team.result = attrs[:result].to_i
       team.save
     end
 
-    match.game_map = GameMap.find_or_create_by(name: match_params[:map]) if match_params[:map]
+    match.game_map = map_params && GameMap.find_or_create_by(name: map_params)
     match.save
 
-    render json: match.to_json, status: :ok
+    render json: match.id.to_json, status: :ok
+  end
+
+  private
+
+  def map_params
+    match_params[:map]
   end
 
   def match_params
-    params.require(:match).permit(:map, { teams: {} }) 
+    params.require(:match).permit(:winner, :map, { teams: {} })
   end
 end
