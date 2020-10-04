c5e74bdc4565642305619fa86396efacf4afadc6
Sheldon Johnson <shayolden@hotmail.com>
2020-10-04 21:13:36 +1100

    create players index

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,20 @@
 class Player < ApplicationRecord
   has_and_belongs_to_many :teams
   has_many :matches, through: :teams
+
+  def match_count
+    matches.count
+  end
+
+  def win_count
+    teams.where(result: 1).count
+  end
+
+  def loss_count
+    teams.where(result: -1).count
+  end
+
+  def draw_count
+    teams.where(result: 0).count
+  end
 end
