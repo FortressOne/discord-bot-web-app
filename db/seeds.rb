59088a1360f635e4ad1500d5f8e6425932dc3ef1
Sheldon Johnson <shayolden@hotmail.com>
2020-10-05 23:31:23 +1100

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+16.times do
+  Player.create(
+    discord_id: Faker::Number.number(digits: 18),
+    name: Faker::Name.first_name
+  )
+end
+
+128.times do
+  players = Player.all.sample(8)
+  match = Match.create
+
+  match.game_map = GameMap.create(
+    name: ["2fort5r", "mbasesr", "bam5", "well6"].sample
+  )
+
+  result = [[1,-1], [0, 0], [-1, 1]].sample
+
+  match.teams.create(
+    name: 1,
+    result: result[0],
+    players: players[0..3]
+  )
+
+  match.teams.create(
+    name: 2,
+    result: result[1],
+    players: players[4..7]
+  )
+end
