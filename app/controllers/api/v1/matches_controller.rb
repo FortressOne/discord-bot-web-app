49a9b43c05215172c5bb9f6a634bdd4b21dbe711
Sheldon Johnson <shayolden@hotmail.com>
2020-10-08 00:39:44 +1100

    Copy code from ratings script for now

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -22,6 +22,57 @@ class Api::V1::MatchesController < ActionController::API
     end
 
     match.game_map = map_params && GameMap.find_or_create_by(name: map_params)
+
+    match.save
+
+    team1 = match.teams.find_by(name: 1)
+    team2 = match.teams.find_by(name: 2)
+
+    team1_rank = case team1.result
+                 when 1 then 1
+                 when 0 then 1
+                 when -1 then 2
+                 end
+
+    team2_rank = case team2.result
+                 when 1 then 1
+                 when 0 then 1
+                 when -1 then 2
+                 end
+
+    team1_players = team1.players
+    team2_players = team2.players
+
+    team1_player_ratings = team1_players.map do |player|
+      Rating.new(player.trueskill_rating.skill, player.trueskill_rating.deviation)
+    end
+
+    team2_player_ratings = team2_players.map do |player|
+      Rating.new(player.trueskill_rating.skill, player.trueskill_rating.deviation)
+    end
+
+    FactorGraph.new(
+      team1_player_ratings => team1_rank,
+      team2_player_ratings => team2_rank
+    ).update_skills
+
+    team1_players.each_with_index do |player, i|
+      rating = team1_player_ratings[i]
+      player.trueskill_rating.skill = rating.mean
+      player.trueskill_rating.deviation = rating.deviation
+      player.trueskill_rating.save
+      player.save
+    end
+
+    team2_players.each_with_index do |player, i|
+      rating = team2_player_ratings[i]
+      player.trueskill_rating.skill = rating.mean
+      player.trueskill_rating.deviation = rating.deviation
+      player.trueskill_rating.save
+      player.save
+    end
+
+    match.ratings_processed = true
     match.save
 
     render json: match.id.to_json, status: :ok
