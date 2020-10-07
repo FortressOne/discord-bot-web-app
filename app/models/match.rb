3514c7d6cc6500a2900b27951785b7a34b35a158
Sheldon Johnson <shayolden@hotmail.com>
2020-10-08 01:34:46 +1100

    Destroy teams along with match

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,6 @@
 class Match < ApplicationRecord
   belongs_to :game_map, optional: true
-  has_many :teams
+  has_many :teams, dependent: :destroy
   has_many :players, through: :teams
 
   scope :ratings_not_processed, -> { where(ratings_processed: nil) }
