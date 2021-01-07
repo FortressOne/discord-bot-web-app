1f41b7767c32eadfbb2faffa7530939306923b17
Sheldon Johnson <shayolden@hotmail.com>
2021-01-08 00:33:01 +1100

    By default return teams and matches by descending created_at date order

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,6 +4,8 @@ class Match < ApplicationRecord
   has_many :teams, dependent: :destroy
   has_many :players, through: :teams
 
+  default_scope { order(created_at: :desc) }
+
   scope :ratings_not_processed, -> { where(ratings_processed: nil) }
   scope :history, -> { includes(:game_map).includes(:discord_channel).includes(teams: :players).order('id DESC') }
 
