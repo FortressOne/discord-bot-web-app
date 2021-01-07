9a125685cb1be9fbf5d41e2e3a2859c3a460a7f2
Sheldon Johnson <shayolden@hotmail.com>
2021-01-08 00:34:25 +1100

    By default return teams and matches by ascending created_at date order

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,5 @@ class Team < ApplicationRecord
   belongs_to :match
   has_and_belongs_to_many :players
 
-  default_scope { order(created_at: :desc) }
+  default_scope { order(created_at: :asc) }
 end
