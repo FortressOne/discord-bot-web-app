737518904e5dbdb3a12479a60f4d4dd86a507629
drzel <shayolden@hotmail.com>
2021-03-02 21:29:08 +1100

    WIP

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,22 @@ class Team < ApplicationRecord
   has_and_belongs_to_many :players
 
   default_scope { order(created_at: :asc) }
+
+  def rank
+    case result
+    when 1 then 1
+    when 0 then 1
+    when -1 then 2
+    end
+  end
+
+  def player_ratings(discord_channel_id)
+    players.map do |player|
+      trueskill_rating = player.trueskill_ratings.find_by(
+        discord_channel_id: discord_channel_id
+      )
+
+      Rating.new(trueskill_rating.mean, trueskill_rating.deviation
+    end
+  end
 end
