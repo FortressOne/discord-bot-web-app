043acd804c7f180b1759044967e8f31956d72805
drzel <shayolden@hotmail.com>
2021-03-04 00:48:07 +1100

    Naively fix PWLD counts

--- app/models/discord_channel.rb
+++ app/models/discord_channel.rb
@@ -1,5 +1,6 @@
 class DiscordChannel < ApplicationRecord
   has_many :matches
   has_many :trueskill_ratings
+  has_many :discord_channel_players
   has_many :players, through: :discord_channel_players
 end
