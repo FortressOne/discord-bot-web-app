45c585b857ee74f23339465dd50a108c7047c045
drzel <shayolden@hotmail.com>
2021-03-04 17:04:07 +1100

    Don't show mean and deviation in leaderboard

--- app/controllers/discord_channels_controller.rb
+++ app/controllers/discord_channels_controller.rb
@@ -1,7 +1,7 @@
 class DiscordChannelsController < ApplicationController
   def show
     @discord_channel = DiscordChannel
-      .includes(players: { matches: :discord_channel })
+      .includes(players: :matches)
       .find_by(channel_id: params[:id])
   end
 end
