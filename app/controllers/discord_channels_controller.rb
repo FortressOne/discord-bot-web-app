1387c075f80215bb04fa29669e2e3ece29bb5dc4
drzel <shayolden@hotmail.com>
2021-03-06 22:40:01 +1100

    Hide invisible players

--- app/controllers/discord_channels_controller.rb
+++ app/controllers/discord_channels_controller.rb
@@ -1,11 +1,8 @@
 class DiscordChannelsController < ApplicationController
   def show
     @discord_channel_players = DiscordChannel
-      .includes(discord_channel_players: [:trueskill_rating, :player])
       .find_by(channel_id: params[:id])
       .discord_channel_players
-      .sort_by do |dcp|
-        dcp.trueskill_rating.conservative_skill_estimate * -1
-      end
+      .leaderboard
   end
 end
--- app/models/discord_channel.rb
+++ app/models/discord_channel.rb
@@ -2,9 +2,4 @@ class DiscordChannel < ApplicationRecord
   has_many :matches
   has_many :discord_channel_players
   has_many :players, through: :discord_channel_players
-
-  scope :leaderboard, -> do
-    includes(players: { matches: :discord_channel })
-      .where.not(players: { name: nil })
-  end
 end
