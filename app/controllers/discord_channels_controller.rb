ebb5e7d9e8a2c00c924897225c4567dacdce1de6
drzel <shayolden@hotmail.com>
2021-03-07 01:42:02 +1100

    Add discord channels index

--- app/controllers/discord_channels_controller.rb
+++ app/controllers/discord_channels_controller.rb
@@ -1,4 +1,8 @@
 class DiscordChannelsController < ApplicationController
+  def index
+    @discord_channels = DiscordChannel.all
+  end
+
   def show
     @discord_channel_players = DiscordChannel
       .find_by(channel_id: params[:id])
