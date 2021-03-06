73de91e4bd464f550bd246e200ca47cf07ff3564
drzel <shayolden@hotmail.com>
2021-03-06 22:45:28 +1100

    Removing a discord channel should remove it's players

--- app/models/discord_channel.rb
+++ app/models/discord_channel.rb
@@ -1,5 +1,5 @@
 class DiscordChannel < ApplicationRecord
   has_many :matches
-  has_many :discord_channel_players
+  has_many :discord_channel_players, dependent: :destroy
   has_many :players, through: :discord_channel_players
 end
