38f55b011e6e8bccf597979fb0ff627c8bea56a3
drzel <shayolden@hotmail.com>
2021-03-06 16:58:48 +1100

    Sort by CSE

--- app/controllers/discord_channels_controller.rb
+++ app/controllers/discord_channels_controller.rb
@@ -1,7 +1,11 @@
 class DiscordChannelsController < ApplicationController
   def show
-    @discord_channel = DiscordChannel
-      .includes(:players)
+    @discord_channel_players = DiscordChannel
+      .includes(discord_channel_players: [:trueskill_rating, :player])
       .find_by(channel_id: params[:id])
+      .discord_channel_players
+      .sort_by do |dcp|
+        dcp.trueskill_rating.conservative_skill_estimate * -1
+      end
   end
 end
