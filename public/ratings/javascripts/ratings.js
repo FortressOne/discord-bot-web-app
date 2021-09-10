6f41b4182b543650f47fd64862979a4578b62584
Sheldon Johnson <shayolden@hotmail.com>
2021-09-10 18:31:52 +1000

    Move things around

---
+++ public/ratings/javascripts/ratings.js
@@ -0,0 +1,10 @@
+import LocalTime from "local-time"
+LocalTime.start()
+
+var Tablesort = require("tablesort")
+
+document.addEventListener("turbolinks:load", function() {
+  new Tablesort(document.getElementById('players_table'));
+})
+
+import "tablesort/tablesort"
