9dd9db99c0f65c5cc1d2842be3062a08c425ffad
Sheldon Johnson <shayolden@hotmail.com>
2021-09-12 02:52:56 +1000

    Fix broken link to discord_channels

--- app/models/discord_channel.rb
+++ app/models/discord_channel.rb
@@ -2,4 +2,8 @@ class DiscordChannel < ApplicationRecord
   has_many :matches
   has_many :discord_channel_players, dependent: :destroy
   has_many :players, through: :discord_channel_players
+
+  def to_param
+    channel_id
+  end
 end
