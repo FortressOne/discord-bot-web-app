de04d662227306c0bdd961baa569002bc09df96d
Sheldon Johnson <shayolden@hotmail.com>
2021-09-17 17:52:11 +1000

    Refactor now broken css splitting

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,5 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
+//= link_directory ../stylesheets/application .css
+//= link_directory ../stylesheets/home .css
+//= link_directory ../stylesheets/results .css
 //= link_tree ../../javascript .js
