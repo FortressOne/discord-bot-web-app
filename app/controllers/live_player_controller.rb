2e45a2cbc0300c875277840f1030251e4eb5396a
Sheldon Johnson <shayolden@hotmail.com>
2021-09-20 21:47:59 +1000

    Use nocookie youtube so it works on safari and refactor a little

--- app/controllers/live_player_controller.rb
+++ app/controllers/live_player_controller.rb
@@ -1,14 +1,15 @@
 class LivePlayerController < ApplicationController
-  CLIENT_ID = Rails.application.credentials.twitch[:client_id]
-  CLIENT_SECRET = Rails.application.credentials.twitch[:client_secret]
-  FORTRESSONE_GAME_ID = 1973310673
-  URL = "https://id.twitch.tv/oauth2/token?client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&grant_type=client_credentials"
+  TWITCH_FORTRESSONE_GAME_ID = 1973310673
 
   def show
-    access_token = JSON.parse(Faraday.post(URL).body)["access_token"]
-    headers = {"Authorization" => "Bearer #{access_token}", "Client-Id" => CLIENT_ID}
-    payload = Faraday.get("https://api.twitch.tv/helix/streams", { game_id: FORTRESSONE_GAME_ID }, headers)
-    body = JSON.parse(payload.body)["data"]
-    @channel = body.any? && body.first["user_name"]
+    @twitch_active_fortressone_channel = twitch
+      .streams(TWITCH_FORTRESSONE_GAME_ID)
+      .first
+  end
+
+  private
+
+  def twitch
+    TwitchClient.new
   end
 end
---
+++ app/lib/twitch_client.rb
@@ -0,0 +1,12 @@
+class TwitchClient
+  CLIENT_ID = Rails.application.credentials.twitch[:client_id]
+  CLIENT_SECRET = Rails.application.credentials.twitch[:client_secret]
+  URL = "https://id.twitch.tv/oauth2/token?client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&grant_type=client_credentials"
+
+  def streams(game_id)
+    access_token = JSON.parse(Faraday.post(URL).body)["access_token"]
+    headers = {"Authorization" => "Bearer #{access_token}", "Client-Id" => CLIENT_ID}
+    payload = Faraday.get("https://api.twitch.tv/helix/streams", { game_id: game_id }, headers)
+    JSON.parse(payload.body)["data"]
+  end
+end
