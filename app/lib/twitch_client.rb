2843c593d939e36481a98f64fe40be01cf9f2324
Sheldon Johnson <shayolden@hotmail.com>
2021-09-20 23:22:42 +1000

    Use sentence x is live on Twitch

--- app/lib/twitch_client.rb
+++ app/lib/twitch_client.rb
@@ -7,6 +7,6 @@ class TwitchClient
     access_token = JSON.parse(Faraday.post(URL).body)["access_token"]
     headers = {"Authorization" => "Bearer #{access_token}", "Client-Id" => CLIENT_ID}
     payload = Faraday.get("https://api.twitch.tv/helix/streams", { game_id: game_id }, headers)
-    JSON.parse(payload.body)["data"]
+    JSON.parse(payload.body)["data"].map { |stream| Stream.new(stream) }
   end
 end
