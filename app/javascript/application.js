1aa86e8c133864ec638f3904606bda106c763387
Sheldon Johnson <shayolden@hotmail.com>
2021-09-23 18:46:28 +1000

    Simply include the javascript from the cdn and you're done

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// import LocalTime from "local-time"
+// LocalTime.start()
+
+document.addEventListener("turbo:load", function() {
+	if(document.getElementById('players_table')) {
+		new Tablesort(document.getElementById('players_table'));
+	}
+})
