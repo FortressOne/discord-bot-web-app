c46834086fe724d515d5301b6765d69531d81c66
Sheldon Johnson <shayolden@hotmail.com>
2022-12-12 01:10:22 +1100

    Add player show page

--- app/models/discord_channel.rb
+++ app/models/discord_channel.rb
@@ -1,7 +1,18 @@
 class DiscordChannel < ApplicationRecord
   has_many :matches
   has_many :discord_channel_players, dependent: :destroy
+  has_many :trueskill_ratings, through: :discord_channel_players
   has_many :players, through: :discord_channel_players
+
+  def percentile(discord_channel_player)
+    100.0 * rank(discord_channel_player) / discord_channel_players.count
+  end
+
+  def rank(discord_channel_player)
+    trueskill_ratings
+      .where("mean > ?", discord_channel_player.rating_mean)
+      .count + 1
+  end
 
   def to_param
     channel_id
