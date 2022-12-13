724ef6595f9c5cf2f996266e11951ec31aabaa61
Sheldon Johnson <shayolden@hotmail.com>
2022-12-14 02:07:25 +1100

    Don't include players with no matches played in calculations

--- app/models/discord_channel.rb
+++ app/models/discord_channel.rb
@@ -5,13 +5,17 @@ class DiscordChannel < ApplicationRecord
   has_many :players, through: :discord_channel_players
 
   def percentile(discord_channel_player)
-    100 - 100.0 * rank(discord_channel_player) / discord_channel_players.count
+    100 - 100.0 * rank(discord_channel_player) / players_with_at_least_one_match_played_count
   end
 
   def rank(discord_channel_player)
     1 + trueskill_ratings.count do |tr|
       tr.conservative_skill_estimate > discord_channel_player.conservative_skill_estimate
     end
+  end
+
+  def players_with_at_least_one_match_played_count
+    discord_channel_players.joins(:teams).distinct.count
   end
 
   def to_param
