9d0ae8e94dbb0e5cd2d7d379a452865055b0f226
Sheldon Johnson <shayolden@hotmail.com>
2022-12-27 00:04:04 +1100

    Revert "Fix some n+1s"
    This reverts commit e106e3dd29d13c31226dbb4f08de8109a0dc28b0.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,17 +2,12 @@ class PlayersController < ApplicationController
   add_breadcrumb "Home", :root_path
 
   def index
-    @players = Player
-      .all
-      .sort_by(&:match_count)
-      .reverse
-
+    @players = Player.all.sort_by(&:match_count).reverse
     add_breadcrumb "Players"
   end
 
   def show
     @player = Player.find(params[:id])
-
     add_breadcrumb "Players", players_path
     add_breadcrumb @player.name
   end
