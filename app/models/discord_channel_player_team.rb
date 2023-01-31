91c52e6e1a1aa7f3b9600b427b94a2d249cbdb3c
Sheldon Johnson <shayolden@hotmail.com>
2023-01-31 15:12:20 +1100

    Don't leave orphan records around

--- app/models/discord_channel_player_team.rb
+++ app/models/discord_channel_player_team.rb
@@ -2,7 +2,7 @@ class DiscordChannelPlayerTeam < ApplicationRecord
   has_one :trueskill_rating, as: :trueskill_rateable, dependent: :destroy
   belongs_to :team
   belongs_to :discord_channel_player
-  has_many :discord_channel_player_team_rounds
+  has_many :discord_channel_player_team_rounds, dependent: :destroy
   has_many :rounds, through: :discord_channel_player_team_round
 
   after_create :create_trueskill_rating
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,5 +1,5 @@
 class Round < ApplicationRecord
   belongs_to :match
-  has_many :discord_channel_player_rounds
+  has_many :discord_channel_player_rounds, dependent: :destroy
   has_many :discord_channel_players, through: :disord_channel_player_rounds
 end
