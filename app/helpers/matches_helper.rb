2bfa2c332d960db8a6b331d1baf9b97b010e9600
Sheldon Johnson <shayolden@hotmail.com>
2023-02-09 23:38:45 +1100

    Clean up

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -2,4 +2,16 @@ module MatchesHelper
   def match_summary(match)
     [match.id, match.description, match.size, match.map_name].compact.join(DELIMITER)
   end
+
+  def match_location(match)
+    if match.server 
+      link_to(match.server.name, "qw://#{match.server.address}") 
+    elsif match.discord_channel && match.discord_channel.invite_url
+      link_to(match.discord_channel.name, match.discord_channel.invite_url) 
+    elsif match.discord_channel
+      match.discord_channel.name
+    else 
+      "Unknown location"
+    end 
+  end
 end
