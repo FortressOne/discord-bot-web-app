9d6649815435238983d04e0f4f102cba4e031bfc
Sheldon Johnson <shayolden@hotmail.com>
2023-02-10 00:24:31 +1100

    Add latest result to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+    @matches = Match
+      .order(created_at: :desc)
+      .includes([
+        :discord_channel,
+        :server,
+        :game_map,
+        { teams: { discord_channel_player_teams: [
+          :discord_channel_player_team_rounds,
+          { discord_channel_player: :player },
+        ]} },
+      ]).limit(1)
   end
 end
