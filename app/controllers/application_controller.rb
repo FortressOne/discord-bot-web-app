3695ac88cbb015c7bb3958edfef8c58ab66575e6
Sheldon Johnson <shayolden@hotmail.com>
2023-02-09 23:25:11 +1100

    add pagy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  include Pagy::Backend
+
   def after_sign_in_path_for(resource)
     stored_location_for(resource) ||
       if resource.is_a?(Player)
