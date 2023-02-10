c44d8d22f3d55dff87f3ae53f279d07672545a1d
Sheldon Johnson <shayolden@hotmail.com>
2023-02-11 10:22:34 +1100

    Don't fail if player has no matches played

--- app/models/player.rb
+++ app/models/player.rb
@@ -57,7 +57,7 @@ class Player < ApplicationRecord
   end
 
   def last_match_date
-    teams.last { |team| team.created_at }.created_at
+    teams.any? && teams.last { |team| team.created_at }.created_at
   end
 
   def discord_channel_players_with_matches_played
