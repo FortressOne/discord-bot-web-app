02ba7624564319faa7765badf402e40fad8dd2d4
Sheldon Johnson <shayolden@hotmail.com>
2023-06-02 03:21:18 +1000

    Remove unneeded migration

--- db/migrate/20230528013401_add_timestamps_to_discord_channel_player_team.rb
+++
@@ -1,6 +0,0 @@
-class AddTimestampsToDiscordChannelPlayerTeam < ActiveRecord::Migration[7.0]
-  def change
-    add_column :discord_channel_player_teams, :created_at, :datetime
-    add_column :discord_channel_player_teams, :updated_at, :datetime
-  end
-end
