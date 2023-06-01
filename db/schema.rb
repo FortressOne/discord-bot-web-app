236103e584b6de4977ba68728948f5c78d3c893b
Sheldon Johnson <shayolden@hotmail.com>
2023-06-02 03:24:08 +1000

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_28_013401) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_13_025459) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,8 +65,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_28_013401) do
   create_table "discord_channel_player_teams", force: :cascade do |t|
     t.bigint "discord_channel_player_id", null: false
     t.bigint "team_id", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "discord_channel_players", force: :cascade do |t|
