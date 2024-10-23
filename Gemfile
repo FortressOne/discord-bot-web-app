c8d0476bfbb226deeb5aa315fa3aae1ccd05e190
Sheldon Johnson <shayolden@hotmail.com>
2024-10-23 12:44:44 +1100

    Update to Rails 7.1

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "3.0.1"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 7.0.3"
+gem "rails", "~> 7.1"
 
 # Use postgresql as the database for Active Record
 gem "pg"
@@ -73,8 +73,6 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
-
-  gem "bullet"
 end
 
 group :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,13 +6,16 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.enable_reloading = true
 
   # Do not eager load code on boot.
   config.eager_load = false
 
   # Show full error reports.
   config.consider_all_requests_local = true
+
+  # Enable server timing
+  config.server_timing = true
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -55,6 +58,9 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Highlight code that enqueued background job in logs.
+  config.active_job.verbose_enqueue_logs = true
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
@@ -67,48 +73,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "host.docker.internal"
-  config.web_console.permissions = '172.20.0.2'
-
-  config.discord = { server_id: "744911007859736616" }
-
-  config.team_emojis = {
-    1 => "<:flag_1:1062548481576800307>",
-    2 => "<:flag_2:1062548478422679625>",
-    3 => "<:flag_3:1062548475402788954>",
-    4 => "<:flag_4:1062548473163034625>",
-    "blank" => "<:blank:1063494588158988318>",
-  }
-
-  config.playerclass_emojis = {
-    blue: {
-      1 => "<:blue_scout:1065445775674462219>",
-      2 => "<:blue_sniper:1065445777649963019>",
-      3 => "<:blue_soldier:1065445781013794897>",
-      4 => "<:blue_demoman:1065445762575642624>",
-      5 => "<:blue_medic:1065445770045698069>",
-      6 => "<:blue_hwguy:1065445767424262164>",
-      7 => "<:blue_pyro:1065445772293832834>",
-      8 => "<:blue_spy:1065445782657962106>",
-      9 => "<:blue_engineer:1065445763779395645>",
-    },
-    red: {
-      1 => "<:red_scout:1065445797660999750>",
-      2 => "<:red_sniper:1065445799183536228>",
-      3 => "<:red_soldier:1065445802190852137>",
-      4 => "<:red_demoman:1065445785518493706>",
-      5 => "<:red_medic:1065445791587631164>",
-      6 => "<:red_hwguy:1065445790060924938>",
-      7 => "<:red_pyro:1065445794167140414>",
-      8 => "<:red_spy:1065445853617209414>",
-      9 => "<:red_engineer:1065445788488060928>",
-    }
-  }
-
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.alert = true
-    Bullet.console = true
-    Bullet.skip_html_injection = false
-  end
+  # Raise error when a before_action's only/except options reference missing actions
+  config.action_controller.raise_on_missing_callback_actions = true
 end
