221d6383d9c92098f16547116f58e2530e1b4043
Sheldon Johnson <shayolden@hotmail.com>
2024-10-24 23:59:22 +1100

    Add healthcheck route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
   devise_for :players, controllers: { omniauth_callbacks: 'players/omniauth_callbacks' }
   devise_scope :player do
     delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_player_session
