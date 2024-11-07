70d99e738223d8f7bd8d73990beb7d74869300af
Sheldon Johnson <shayolden@hotmail.com>
2024-11-07 12:53:24 +1100

    More logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
 
+  before_action :log_request_format
+
   def after_sign_in_path_for(resource)
     stored_location_for(resource) ||
       if resource.is_a?(Player)
@@ -8,5 +10,14 @@ class ApplicationController < ActionController::Base
       else
         super
       end
+  end
+
+  private
+
+  def log_request_format
+    Rails.logger.info("Headers: #{request.headers.to_h}")
+    Rails.logger.info("Request format: #{request.format}")
+    Rails.logger.info("Parameters: #{params.to_unsafe_h}")
+    Rails.logger.info("User Agent: #{request.user_agent}")
   end
 end
