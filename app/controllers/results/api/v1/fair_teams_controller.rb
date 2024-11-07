f89435590c5c10a695d94d05beabd16ee2fd9d0a
Sheldon Johnson <shayolden@hotmail.com>
2024-11-07 12:04:18 +1100

    More logging

--- app/controllers/results/api/v1/fair_teams_controller.rb
+++ app/controllers/results/api/v1/fair_teams_controller.rb
@@ -4,9 +4,13 @@ class Results::Api::V1::FairTeamsController < ApplicationController
   include Saulabs::TrueSkill
 
   def new
+    puts("AAAAAAAAAAAAAAAAAAAAAAA")
+
     discord_channel = DiscordChannel.find_or_create_by(
       channel_id: discord_channel_id_params
     )
+
+    puts("BBBBBBBBBBBBBBBBBBBB")
 
     discord_channel_players = players_params.map do |discord_id|
       DiscordChannelPlayer.find_or_create_by(
@@ -17,8 +21,12 @@ class Results::Api::V1::FairTeamsController < ApplicationController
 
     teamsize = discord_channel_players.size / 2
 
+    puts("CCCCCCCCCCCCCCCCCCCCcc")
+
     player1 = discord_channel_players.shift
     team1_combinations = discord_channel_players.combination(teamsize - 1)
+
+    puts("DDDDDDDDDDDDDDDDDDDDDDD")
 
     matchups = team1_combinations.inject([]) do |matchups, combo|
       team1 = combo + [player1]
@@ -41,13 +49,19 @@ class Results::Api::V1::FairTeamsController < ApplicationController
       matchups << teams
     end
 
+    puts("EEEEEEEEEEEEEEEEEEEEEEEEEEE")
+
     sorted_matchups = matchups.sort_by do |matchup|
       Matchup.new(matchup).difference
     end
 
+    puts("FFFFFFFFFFFFFFFFFFFFFFF")
+
     results = sorted_matchups.inject([]) do |results, matchup|
       results << Matchup.new(matchup).teams
     end
+
+    puts("GGGGGGGGGGGGGGGGGGGGGGGGGGG")
 
     puts("==============================")
     puts(results.to_json)
